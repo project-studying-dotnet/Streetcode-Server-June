62cd39fa26dcaed385952c3af9f5910f07e6af4d
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-10 16:55:54 +0300

    fixed errors in email services

--- Streetcode/Streetcode.DAL/Entities/AdditionalContent/Email/EmailConfiguration.cs
+++ Streetcode/Streetcode.DAL/Entities/AdditionalContent/Email/EmailConfiguration.cs
@@ -4,7 +4,7 @@ namespace Streetcode.DAL.Entities.AdditionalContent.Email
     {
         public string? From { get; set; }
         public string? SmtpServer { get; set; }
-        public int? Port { get; set; }
+        public int Port { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }
     }
