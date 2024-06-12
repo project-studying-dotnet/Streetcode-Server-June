b84535f30adc847a14c884b2e5d1ee9da9e0aa28
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-13 00:33:03 +0300

    updated createSourceCategoryDTO

--- Streetcode/Streetcode.BLL/DTO/Sources/Create/CreateSourceCategoryDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Sources/Create/CreateSourceCategoryDTO.cs
@@ -9,8 +9,12 @@ namespace Streetcode.BLL.DTO.Sources.Create
     public class CreateSourceCategoryDTO
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+        [Required]
         public int ImageId { get; set; }
-        public ImageDTO? Image { get; set; }
+        [Required]
+        public ImageDTO Image { get; set; }
     }
 }
