74dcaa859b2b33d9ff0531346e9b3b0fbba929fe
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-13 18:34:59 +0300

    remove non nullable at DTO

--- Streetcode/Streetcode.BLL/DTO/Media/Audio/AudioDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Media/Audio/AudioDTO.cs
@@ -6,7 +6,7 @@ public class AudioDTO
 {
   public int Id { get; set; }
   public string? Description { get; set; }
-  public string BlobName { get; set; }
-  public string Base64 { get; set; }
-  public string MimeType { get; set; }
+  public string BlobName { get; set; } = string.Empty;
+  public string Base64 { get; set; } = string.Empty;
+  public string MimeType { get; set; } = string.Empty;
 }
--- Streetcode/Streetcode.BLL/DTO/Team/TeamMemberDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Team/TeamMemberDTO.cs
@@ -6,9 +6,9 @@ namespace Streetcode.BLL.DTO.Team
     public class TeamMemberDTO
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Description { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public bool IsMain { get; set; }
         public int ImageId { get; set; }
         public List<TeamMemberLinkDTO> TeamMemberLinks { get; set; } = new List<TeamMemberLinkDTO>();
