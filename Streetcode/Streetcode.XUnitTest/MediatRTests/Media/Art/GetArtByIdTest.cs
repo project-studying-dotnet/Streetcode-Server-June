68fb77782259f75cae676bbc9fcf0783fd67bfb7
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-06-14 01:33:28 +0300

    update

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Art/GetArtByIdTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Art/GetArtByIdTest.cs
@@ -1,10 +1,14 @@
 ﻿namespace Streetcode.XUnitTest.MediatRTests.Media.Art;
 using AutoMapper;
 using Moq;
+using Streetcode.BLL.Enums;
+using Streetcode.BLL.Exceptions;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.MediatR.Media.Art.GetById;
 using Streetcode.DAL.Repositories.Interfaces.Base;
+using System;
 using System.Linq.Expressions;
+using System.Net;
 using Xunit;
 public class GetArtByIdHandlerTests
 {
@@ -54,9 +58,11 @@ public class GetArtByIdHandlerTests
             .ReturnsAsync(null as DAL.Entities.Media.Images.Art);
 
         // Act
-        var result = await handler.Handle(request, CancellationToken.None);
+        var result = await Assert.ThrowsAsync<EntityNotFoundException>(() => handler.Handle(request, CancellationToken.None));
 
         // Assert
-        Assert.False(result.IsSuccess);
+        Assert.Equal(ErrorType.NotFound, result.ErrorType);
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+        Assert.Equal("Entity 'Art' with id '1' not found", result.Message);
     }
 }
