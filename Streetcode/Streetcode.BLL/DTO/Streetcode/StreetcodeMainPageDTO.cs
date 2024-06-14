0cd68f056c40eeecd01118efbf6ffde8b08a6d4d
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-14 14:03:15 +0300

    remove non nullable at dto

--- Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeMainPageDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeMainPageDTO.cs
@@ -11,12 +11,12 @@ namespace Streetcode.BLL.DTO.Streetcode
     public class StreetcodeMainPageDTO
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         public string? Alias { get; set; }
         public string? Teaser { get; set; }
         public string? Text { get; set; }
         public int ImageId { get; set; }
 
-        public string TransliterationUrl { get; set; }
+        public string TransliterationUrl { get; set; } = string.Empty;
     }
 }
