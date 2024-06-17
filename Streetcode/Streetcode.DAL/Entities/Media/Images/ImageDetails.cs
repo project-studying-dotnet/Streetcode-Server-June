36b5c712c74a5c22d39a93eed2f14bbbf5c4d88f
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-17 14:41:20 +0300

    remove =new to image test add string empty to alt and title

--- Streetcode/Streetcode.DAL/Entities/Media/Images/ImageDetails.cs
+++ Streetcode/Streetcode.DAL/Entities/Media/Images/ImageDetails.cs
@@ -11,14 +11,14 @@ namespace Streetcode.DAL.Entities.Media.Images
         public int Id { get; set; }
 
         [MaxLength(100)]
-        public string? Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
         [MaxLength(300)]
-        public string? Alt { get; set; }
+        public string Alt { get; set; } = string.Empty;
 
         [Required]
         public int ImageId { get; set; }
 
-        public Image Image { get; set; } = new();
+        public Image? Image { get; set; }
     }
 }
