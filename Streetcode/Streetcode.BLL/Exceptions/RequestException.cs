19d9d0db5bd6e4e4071cbac2613fc3a472cc6bdd
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-06-17 14:29:12 +0300

    update RequestException

--- Streetcode/Streetcode.BLL/Exceptions/RequestException.cs
+++ Streetcode/Streetcode.BLL/Exceptions/RequestException.cs
@@ -4,20 +4,17 @@ namespace Streetcode.BLL.Exceptions;
 
 public class RequestException : Exception
 {
+    public RequestException(HttpStatusCode statusCode)
+    {
+        StatusCode = statusCode;
+    }
+
     public RequestException(string message, HttpStatusCode statusCode) : base(message)
     {
         StatusCode = statusCode;
     }
 
-    public RequestException()
-    {
-    }
-
-    public RequestException(string message) : base(message)
-    {
-    }
-
-    public RequestException(string message, Exception inner) : base(message, inner)
+    public RequestException(string message, Exception inner, HttpStatusCode statusCode) : base(message, inner)
     {
     }
 
