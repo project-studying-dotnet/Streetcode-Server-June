35991b0cc011110fd7ad9636faf6f83a634142c9
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-07-01 23:40:41 +0300

    delete redundant LoginResultDTO

--- Streetcode/Streetcode.BLL/DTO/Users/LoginResultDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Streetcode.BLL.DTO.Users
-{
-    public class LoginResultDTO
-    {
-        public UserDTO User { get; set; }
-        public string AccessToken { get; set; } = string.Empty;
-        public RefreshTokenDTO RefreshToken { get; set; } 
-    }
-}
