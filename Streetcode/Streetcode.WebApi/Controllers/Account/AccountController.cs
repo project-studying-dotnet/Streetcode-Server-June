3c9fb9bed1f8a7c87b2111cdb6df37f2ad40d04d
Pavlo Ieremeiew <peremuv3@gmail.com>
2024-07-02 18:54:08 +0300

    fix/197 : fix controller

--- Streetcode/Streetcode.WebApi/Controllers/Account/AccountController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Account/AccountController.cs
@@ -13,6 +13,24 @@ namespace Streetcode.WebApi.Controllers.Account
         {
             return HandleResult(await Mediator.Send(new RegisterUserCommand(newUser)));
         }
+        
+        [HttpPost]
+        public async Task<ActionResult<string>> RefreshTokens(TokenResponseDTO response)
+        {
+            return HandleResult(await Mediator.Send(new RefreshTokensCommand(response)));
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            return HandleResult(await Mediator.Send(new LogoutUserCommand()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO loginUser)
+        {
+            return HandleResult(await Mediator.Send(new LoginUserCommand(loginUser)));
+        }
 
         [HttpGet]
         public async Task<IActionResult> ConfirmEmail([FromRoute]string userId, [FromRoute]string token)
