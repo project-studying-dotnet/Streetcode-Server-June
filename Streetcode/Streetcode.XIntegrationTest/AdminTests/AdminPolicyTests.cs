6d7204d5fec79b749ee586b45c2f360f48ff22df
Forestieur <Forestieur@gmail.com>
2024-07-08 08:30:39 +0300

    add test

---
+++ Streetcode/Streetcode.XIntegrationTest/AdminTests/AdminPolicyTests.cs
@@ -0,0 +1,183 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Streetcode.BLL.Interfaces.Users;
+using Streetcode.DAL.Entities.Users;
+using Xunit;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+
+public class AdminPolicyTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private HttpClient _client;
+    private CustomWebApplicationFactory<Program> _factory;
+
+    public AdminPolicyTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Create_Position_AuthorizedAsAdmin_ReturnsOk()
+    {
+        // Arrange
+        var token = await GetAdminToken();
+        AddAuthorizationHeader(_client, token);
+        var content = new StringContent("{\"position\":\"New Fact\"}", Encoding.UTF8, "application/json");
+        var contentString = await content.ReadAsStringAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/Position/Create")
+        {
+            Content = content
+        };
+        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        // Act
+        var response = await _client.SendAsync(request);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var positionResponse = JsonSerializer.Deserialize<PositionDTO>(responseContent, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains(positionResponse.Position, contentString);
+        });
+    }
+
+    [Fact]
+    public async Task Create_Position_Unauthorized_ReturnsUnauthorized()
+    {
+        // Arrange
+        var token = "SomeInvalidToken"; // Simulate non-admin user
+        AddAuthorizationHeader(_client, token);
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/Position/Create")
+        {
+            Content = new StringContent("{\"Position\": \"New Fact\"}", Encoding.UTF8, "application/json")
+        };
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Delete_Fact_AuthorizedAsAdmin_ReturnsOk()
+    {
+        // Arrange
+        var token = await GetAdminToken(); // simulate admin user
+        AddAuthorizationHeader(_client, token);
+        var factId = 1;
+        var request = new HttpRequestMessage(HttpMethod.Delete, $"/api/Fact/Delete/{factId}");
+
+        // Act
+        var response = await _client.SendAsync(request);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var positionResponse = JsonSerializer.Deserialize<FactDto>(responseContent, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(positionResponse.Id, factId);
+        });
+    }
+
+    [Fact]
+    public async Task Delete_Fact_Unauthorized_ReturnsUnauthorized()
+    {
+        // Arrange
+        var token = "SomeInvalidToken"; // Simulate non-admin user
+        AddAuthorizationHeader(_client, token);
+        var request = new HttpRequestMessage(HttpMethod.Delete, "/api/Fact/Delete/1");
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Update_Term_AuthorizedAsAdmin_ReturnsOk()
+    {
+        // Arrange
+        var token = await GetAdminToken();
+        AddAuthorizationHeader(_client, token);
+        var content = new StringContent("{\"id\":1,\"title\":\"value\",\"description\":\"value\"}", Encoding.UTF8, "application/json");
+        var contentString = content.ReadAsStringAsync();
+        var request = new HttpRequestMessage(HttpMethod.Put, "/api/Term/Update")
+        {
+            Content = content
+        };
+
+        // Act
+        var response = await _client.SendAsync(request);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(contentString.Result, responseContent);
+        });
+    }
+
+    [Fact]
+    public async Task Update_Term_Unauthorized_ReturnsUnauthorized()
+    {
+        // Arrange
+        var token = "SomeInvalidToken"; // Simulate non-admin user
+        AddAuthorizationHeader(_client, token);
+        var request = new HttpRequestMessage(HttpMethod.Put, "/api/Term/Update")
+        {
+            Content = new StringContent("{\"title\": \"value\"}", Encoding.UTF8, "application/json")
+        };
+
+        // Act
+        var response = await _client.SendAsync(request);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    private async Task<string> GetAdminToken()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+        var tokenService = scope.ServiceProvider.GetRequiredService<ITokenService>();
+        var token = await LoginAdminUserAsync(userManager, tokenService);
+        return token;
+    }
+
+    private async Task<string> LoginAdminUserAsync(UserManager<User> userManager, ITokenService tokenService)
+    {
+        const string adminUserName = "SuperAdmin";
+
+        var adminUser = await userManager.FindByNameAsync(adminUserName);
+        if (adminUser == null)
+        {
+            throw new Exception("Admin user not found.");
+        }
+
+        var claims = await tokenService.GetUserClaimsAsync(adminUser);
+        var token = tokenService.GenerateAccessToken(adminUser, claims);
+        return token;
+    }
+
+    private void AddAuthorizationHeader(HttpClient client, string token)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+}
---
+++ Streetcode/Streetcode.XIntegrationTest/AdminTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Streetcode.DAL.Entities.Users;
+using Streetcode.DAL.Persistence;
+using Streetcode.WebApi.Extensions;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram>
+    where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices((context, services) =>
+        {
+            // Remove the app's StreetcodeDbContext registration.
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<StreetcodeDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Remove the AddApplicationServices registration
+            var applicationServiceDescriptor = services.SingleOrDefault(
+                d => d.ImplementationFactory != null && d.ImplementationFactory.Method.Name.Contains("AddApplicationServices"));
+            if (applicationServiceDescriptor != null)
+            {
+                services.Remove(applicationServiceDescriptor);
+            }
+
+            // Add an in-memory database context.
+            services.AddDbContext<StreetcodeDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            // Apply the access token configuration.
+            var authenticationDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(IConfigureOptions<AuthenticationOptions>));
+            if (authenticationDescriptor == null)
+            {
+                services.AddTokensConfiguration(context.Configuration);
+            }
+
+            // Build the service provider.
+            var sp = services.BuildServiceProvider();
+
+            // Create a scope to obtain a reference to the database contexts.
+            using (var scope = sp.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<StreetcodeDbContext>();
+                var userManager = scopedServices.GetRequiredService<UserManager<User>>();
+                var roleManager = scopedServices.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+                // Ensure the database is created.
+                db.Database.EnsureCreated();
+
+                // Seed the database with test data.
+                try
+                {
+                    CustomSeeding.SeedFactsAsync(db).Wait();
+                    CustomSeeding.SeedIdentityDataAsync(userManager, roleManager).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TProgram>>>();
+                    logger.LogError(ex, "An error occurred seeding the database.");
+                }
+            }
+
+            services.Configure<HostOptions>(hostOptions =>
+            {
+                hostOptions.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+            });
+        });
+        builder.UseEnvironment("IntegrationTests");
+
+    }
+}
