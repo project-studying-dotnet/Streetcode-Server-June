d4ddf2d84e41eb517384c854b1ee1b65f7e24ef9
Forestieur <Forestieur@gmail.com>
2024-07-15 11:32:34 +0300

    fix code smells

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Likes/GetLikesByUserHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Likes/GetLikesByUserHandlerTests.cs
@@ -1,24 +1,19 @@
-﻿using AutoMapper;
-using Castle.Core.Logging;
-using Microsoft.AspNetCore.Identity;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Query;
 using Moq;
-using Streetcode.BLL.DTO.Likes;
+using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Interfaces.Users;
 using Streetcode.BLL.MediatR.Likes.PushLike;
 using Streetcode.BLL.Resources;
 using Streetcode.DAL.Entities.Likes;
 using Streetcode.DAL.Entities.Streetcode;
-using Streetcode.BLL.Interfaces.Users;
 using Streetcode.DAL.Entities.Users;
 using Streetcode.DAL.Repositories.Interfaces.Base;
-using System.Linq.Expressions;
 using Xunit;
-using Streetcode.BLL.Services.Tokens;
-using Azure.Core;
-using Castle.Core.Configuration;
-using Streetcode.BLL.DTO.Streetcode;
-using Microsoft.EntityFrameworkCore.Query;
 
 namespace Streetcode.XUnitTest.MediatRTests.Likes
 {
@@ -78,7 +73,6 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
         private Mock<ITokenService> _tokenServiceMock;
         private Mock<UserManager<User>> _userManagerMock;
 
-
         public GetLikesByUserHandlerTests()
         {
             _wrapperMock = new Mock<IRepositoryWrapper>();
@@ -95,8 +89,12 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
         {
             // Arrange
             var handler = new GetLikesByUserHandler(
-                _wrapperMock.Object, _mapperMock.Object, _loggerMock.Object, _httpContextAccessorMock.Object,
-                _tokenServiceMock.Object, _userManagerMock.Object);
+                _wrapperMock.Object,
+                _mapperMock.Object,
+                _loggerMock.Object,
+                _httpContextAccessorMock.Object,
+                _tokenServiceMock.Object,
+                _userManagerMock.Object);
             var request = new GetLikesByUserQuery();
             var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.AccessTokenNotFound, request);
 
@@ -125,8 +123,12 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
         {
             // Arrange
             var handler = new GetLikesByUserHandler(
-               _wrapperMock.Object, _mapperMock.Object, _loggerMock.Object, _httpContextAccessorMock.Object,
-               _tokenServiceMock.Object, _userManagerMock.Object);
+                _wrapperMock.Object,
+                _mapperMock.Object,
+                _loggerMock.Object,
+                _httpContextAccessorMock.Object,
+                _tokenServiceMock.Object,
+                _userManagerMock.Object);
             var request = new GetLikesByUserQuery();
             var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserNotFound, request);
             var cookies = new Mock<IRequestCookieCollection>();
@@ -156,8 +158,12 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
         {
             // Arrange
             var handler = new GetLikesByUserHandler(
-               _wrapperMock.Object, _mapperMock.Object, _loggerMock.Object, _httpContextAccessorMock.Object,
-               _tokenServiceMock.Object, _userManagerMock.Object);
+                _wrapperMock.Object,
+                _mapperMock.Object,
+                _loggerMock.Object,
+                _httpContextAccessorMock.Object,
+                _tokenServiceMock.Object,
+                _userManagerMock.Object);
             var request = new GetLikesByUserQuery();
             var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserNotFound, request);
             var cookies = new Mock<IRequestCookieCollection>();
@@ -184,8 +190,12 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
         {
             // Arrange
             var handler = new GetLikesByUserHandler(
-               _wrapperMock.Object, _mapperMock.Object, _loggerMock.Object, _httpContextAccessorMock.Object,
-               _tokenServiceMock.Object, _userManagerMock.Object);
+                _wrapperMock.Object,
+                _mapperMock.Object,
+                _loggerMock.Object,
+                _httpContextAccessorMock.Object,
+                _tokenServiceMock.Object,
+                _userManagerMock.Object);
             var request = new GetLikesByUserQuery();
             var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserNotFound, request);
             var cookies = new Mock<IRequestCookieCollection>();
@@ -197,7 +207,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Likes
             httpContextMock.Setup(c => c.Request).Returns(requestMock.Object);
             _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
             _tokenServiceMock.Setup(ts => ts.GetUserIdFromAccessToken(It.IsAny<string>())).Returns(string.Empty);
-            _userManagerMock.Setup(um => um.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new User() {Id = _id });
+            _userManagerMock.Setup(um => um.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new User() { Id = _id });
             _wrapperMock.Setup(wrapper => wrapper.LikeRepository.GetAllAsync(
             It.IsAny<Expression<Func<Like, bool>>>(),
             It.IsAny<Func<IQueryable<Like>, IIncludableQueryable<Like, object>>>())).ReturnsAsync(_likes.Where(l => l.UserId == _id));
