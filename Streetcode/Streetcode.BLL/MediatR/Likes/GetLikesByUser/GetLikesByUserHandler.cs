ea2df59d47434a6976fbe3349f6dd2b10469aa12
Forestieur <Forestieur@gmail.com>
2024-07-15 09:27:25 +0300

    change naming, add test

--- Streetcode/Streetcode.BLL/MediatR/Likes/GetLikesByUser/GetLikesByUserHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Likes/GetLikesByUser/GetLikesByUserHandler.cs
@@ -4,18 +4,16 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Streetcode.BLL.DTO.Likes;
-using Streetcode.BLL.DTO.Media.Art;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.Interfaces.Users;
 using Streetcode.BLL.Resources;
 using Streetcode.DAL.Entities.Likes;
-using Streetcode.DAL.Entities.Media.Images;
 using Streetcode.DAL.Entities.Users;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Likes.PushLike
 {
-    public class GetLikesByUser : IRequestHandler<GetLikesByUserQuery, Result<IEnumerable<LikeDTO>>>
+    public class GetLikesByUserHandler : IRequestHandler<GetLikesByUserQuery, Result<IEnumerable<LikeDTO>>>
     {
         private IRepositoryWrapper _wrapper;
         private ILoggerService _logger;
@@ -24,7 +22,7 @@ namespace Streetcode.BLL.MediatR.Likes.PushLike
         private ITokenService _tokenService;
         private UserManager<User> _userManager;
 
-        public GetLikesByUser(
+        public GetLikesByUserHandler(
             IRepositoryWrapper wrapper,
             IMapper mapper, 
             ILoggerService logger,
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Likes/GetLikesByUserHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Streetcode.BLL.DTO.Likes;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Likes.PushLike;
+using Streetcode.BLL.Resources;
+using Streetcode.DAL.Entities.Likes;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.BLL.Interfaces.Users;
+using Streetcode.DAL.Entities.Users;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Likes
+{
+    public class GetLikesByUserHandlerTests
+    {
+        private Mock<IRepositoryWrapper> _wrapperMock;
+        private Mock<ILoggerService> _loggerMock;
+        private Mock<IMapper> _mapperMock;
+        private Mock<IHttpContextAccessor> _httpContextAccessorMock;
+        private Mock<ITokenService> _tokenServiceMock;
+        private Mock<UserManager<User>> _userManagerMock;
+
+        public GetLikesByUserHandlerTests()
+        {
+            _wrapperMock = new Mock<IRepositoryWrapper>();
+            _mapperMock = new Mock<IMapper>();
+            _loggerMock = new Mock<ILoggerService>();
+            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            _tokenServiceMock = new Mock<ITokenService>();
+            _userManagerMock = new Mock<UserManager<User>>(
+                new Mock<IUserStore<User>>().Object, null, null, null, null, null, null, null, null);
+        }
+
+        [Fact]
+        public async Task Handler_ShouldReturnFailure_WhenNoAccessToken()
+        {
+            // Arrange
+            var handler = new GetLikesByUserHandler(
+                _wrapperMock.Object, _mapperMock.Object, _loggerMock.Object, _httpContextAccessorMock.Object,
+                _tokenServiceMock.Object, _userManagerMock.Object);
+            var request = new GetLikesByUserQuery();
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.AccessTokenNotFound, request);
+
+            var cookies = new Mock<IRequestCookieCollection>();
+            var httpContextMock = new Mock<HttpContext>();
+            var requestMock = new Mock<HttpRequest>();
+
+            cookies.Setup(c => c.TryGetValue("accessToken", out It.Ref<string?>.IsAny)).Returns(false);
+            requestMock.Setup(r => r.Cookies).Returns(cookies.Object);
+            httpContextMock.Setup(c => c.Request).Returns(requestMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.False(result.IsSuccess);
+                Assert.Equal(errorMsg, result.Errors.FirstOrDefault()?.Message);
+            });
+        }
+    }
+}
